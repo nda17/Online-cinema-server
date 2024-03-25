ae9b79d36c636bd82da53a0d5f2c15ecfba33c76
nfdmitry <khv1702@gmail.com>
2024-03-25 13:21:02 +0300

    Jwt configuration

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -14,8 +14,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 	) {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-			ignoreExpiration: true,
 			secretOrKey: configService.get('JWT_SECRET')
+			// ignoreExpiration: true
 		})
 	}
 
