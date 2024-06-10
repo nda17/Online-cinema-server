897c6503f9f054df5fa1df42658ac49553ffe8d8
nfdmitry <khv1702@gmail.com>
2024-06-11 02:20:23 +0300

    The pattern for validating the entered password using a regular expression has been changed

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,10 +1,13 @@
-import { IsEmail, IsString, MinLength } from 'class-validator'
+import { IsEmail, IsString, Matches } from 'class-validator'
 
 export class AuthDto {
 	@IsEmail()
 	email: string
 
-	@MinLength(6, { message: 'Password cannot be less than 6 characters!' })
+	@Matches(/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}/g, {
+		message:
+			'Min length should more 6 symbols. Contains 1 number 0-9, 1 Latin letter a-z, 1 Latin letter A-Z'
+	})
 	@IsString()
 	password: string
 }
