7350736d446abae8232fa2b22100e555130d7495
nfdmitry <khv1702@gmail.com>
2024-06-14 01:18:24 +0300

    Added processing of user requests to reset passwords

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,6 +7,7 @@ import {
 	ValidationPipe
 } from '@nestjs/common'
 //eslint-disable-next-line
+import { UpdateUserDto } from '@user/dto/update-user.dto'
 import { AuthService } from './auth.service'
 import { AuthDto } from './dto/auth.dto'
 import { RefreshTokenDto } from './dto/refreshToken.dto'
@@ -14,6 +15,12 @@ import { RefreshTokenDto } from './dto/refreshToken.dto'
 @Controller('auth')
 export class AuthController {
 	constructor(private readonly AuthService: AuthService) {}
+
+	@Post('restore-password')
+	@HttpCode(200)
+	async restorePassword(@Body() dto: UpdateUserDto) {
+		return this.AuthService.restorePassword(dto)
+	}
 
 	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
