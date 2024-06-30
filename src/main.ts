7a4bba6ed1613190511af3bd5d8fa194e72f02c0
nfdmitry <khv1702@gmail.com>
2024-06-30 05:16:51 +0300

    Added logging of requests, changed the color of the server startup status text

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,27 @@
 import { NestFactory } from '@nestjs/core'
+import 'colors'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.setGlobalPrefix('api')
+
+	if (process.env.NODE_ENV === 'development') {
+		app.use(require('morgan')('dev'))
+	}
+
 	app.enableCors({
 		origin: '*',
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 		credentials: true
-	}) //Enable CORS
+	})
 
 	const PORT = process.env.PORT || 5000
 
 	await app.listen(PORT, () =>
 		console.log(
 			`🚀🚀🚀 Server running in ${process.env.NODE_ENV} mode at http://localhost:${PORT} 🚀🚀🚀`
+				.white.bgRed.bold
 		)
 	)
 }
